2bc195f6748679fa9cd65f37ebcdc05c73c80c53
Lalaudomal <lucasdark30@gmail.com>
2022-10-21 11:28:06 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Balta.ContentContext
+{
+
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var Course = new Course();
+        }
+    }
+}
---
+++ contentcontext/Course.cs
@@ -0,0 +1,30 @@
+namespace Balta.ContentContext
+{
+    public class Course : Content
+    {
+        public Course()
+        {
+            Modules = new List<Module>();
+        }
+        public string Tag { get; set; }
+        public IList<Module> Modules { get; set; }
+
+    }
+    public class Module : Content
+    {
+        public Module()
+        {
+            Lectures = new List<Lecture>();
+        }
+        public int Order { get; set; }
+
+        public string Title { get; set; }
+        public IList<Lecture> Lectures { get; set; }
+    }
+
+    public class Lecture
+    {
+        public int Ordem { get; set; }
+        public string Title { get; set; }
+    }
+}
